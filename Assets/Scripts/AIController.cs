05df5b4e1b8919fe827ff1bd817461d49867ae4e
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-04-16 23:02:26 +0300

    feat(AI): Create AI system basic. #.

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public GameObject Player;
+    [SerializeField] private float enemyDistanceRun = 4.0f;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        Debug.Log("Distance: " + distance);
+
+        if (distance < enemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position + dirToPlayer;
+            _agent.SetDestination(newPos);
+        }
+    }
+}
